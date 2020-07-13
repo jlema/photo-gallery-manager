24967d3a91c28fc713ecf966b7488c45f956fccf
John (Juan) Lema <jlema@users.noreply.github.com>
2020-07-12 23:03:09 -0700

    Fixed routing errors that were breaking SPA

--- client/src/components/GalleryList.js
+++ client/src/components/GalleryList.js
@@ -1,42 +1,46 @@
 // eslint-disable-next-line
 
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startListGalleries } from '../actions/galleries';
 
 const GalleryList = ({ errors, galleries, dispatch }) => {
-    const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        setIsLoading(true);
-        dispatch(startListGalleries());
-    }, []);
+	useEffect(() => {
+		setIsLoading(true);
+		dispatch(startListGalleries());
+	}, []);
 
-    useEffect(() => {
-        if (galleries.length > 0) {
-            setIsLoading(false);
-        }
-    }, [galleries]);
+	useEffect(() => {
+		if (galleries.length > 0) {
+			setIsLoading(false);
+		}
+	}, [galleries]);
 
-    return (
-        <div className="galleries-list">
-            {errors && errors.get_error && (
-                <p className="errorMsg centered-message">{errors.get_error}</p>
-            )}
-            {isLoading ? (
-                <div className="loading-msg centered-message">Loading...</div>
-            ) : (
-                    galleries.map((gallery) =>
-                        <li key={gallery._id}>{gallery.name}: <a href={`/gallery/${gallery._id}/`}>{gallery.title}</a></li>
-                    )
-                )}
-        </div>
-    );
+	return (
+		<div className='galleries-list'>
+			{errors && errors.get_error && (
+				<p className='errorMsg centered-message'>{errors.get_error}</p>
+			)}
+			{isLoading ? (
+				<div className='loading-msg centered-message'>Loading...</div>
+			) : (
+				galleries.map(gallery => (
+					<li key={gallery._id}>
+						{gallery.name}:{' '}
+						<Link to={`/gallery/${gallery._id}`}>{gallery.title}</Link>
+					</li>
+				))
+			)}
+		</div>
+	);
 };
 
-const mapStateToProps = (state) => ({
-    galleries: state.galleries.galleries || [],
-    errors: state.errors.errors || {}
+const mapStateToProps = state => ({
+	galleries: state.galleries.galleries || [],
+	errors: state.errors.errors || {}
 });
 
 export default connect(mapStateToProps)(GalleryList);
--- client/src/components/Photo.js
+++ client/src/components/Photo.js
@@ -1,45 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { startLoadPhotoMeta } from '../actions/photos';
 
-const Photo = ({ match, name, caption, id, loadMeta = true, errors, meta, dispatch }) => {
+const Photo = ({
+	match,
+	name,
+	caption,
+	id,
+	loadMeta = true,
+	errors,
+	meta,
+	dispatch
+}) => {
+	id = id ? id : match.params.photoId;
+	const [isLoading, setIsLoading] = useState(false);
 
-  id = id ? id : match.params.photoId;
-  const [isLoading, setIsLoading] = useState(false);
+	useEffect(() => {
+		if (loadMeta) {
+			setIsLoading(true);
+			dispatch(startLoadPhotoMeta(id));
+		}
+	}, []);
 
-  useEffect(() => {
-    if (loadMeta) {
-      setIsLoading(true);
-      dispatch(startLoadPhotoMeta(id));
-    }
-  }, []);
+	useEffect(() => {
+		if (meta) {
+			setIsLoading(false);
+		}
+	}, [meta]);
 
-  useEffect(() => {
-    if (meta) {
-      setIsLoading(false);
-    }
-  }, [meta]);
-
-  return (
-    <Card className="photo">
-      <Card.Img
-        variant="top"
-        src={`/photos/${id}`}
-        alt="Photo"
-      />
-      <Card.Body>
-        <Card.Title>{isLoading ? 'Loading' : meta.name ? meta.name : name}</Card.Title>
-        <Card.Subtitle>{isLoading ? 'Loading' : meta.caption ? meta.caption : caption}</Card.Subtitle>
-        {!loadMeta && <Card.Link href={`/photo/${id}`}>Full Size Photo</Card.Link>}
-      </Card.Body>
-    </Card>
-  );
+	return (
+		<Card className='photo'>
+			<Card.Img variant='top' src={`/photos/${id}`} alt='Photo' />
+			<Card.Body>
+				<Card.Title>
+					{isLoading ? 'Loading' : meta.name ? meta.name : name}
+				</Card.Title>
+				<Card.Subtitle>
+					{isLoading ? 'Loading' : meta.caption ? meta.caption : caption}
+				</Card.Subtitle>
+				{!loadMeta && (
+					<Link to={`/photo/${id}`}>
+						<Card.Link>Full Size Photo</Card.Link>
+					</Link>
+				)}
+			</Card.Body>
+		</Card>
+	);
 };
 
-const mapStateToProps = (state) => ({
-  errors: state.errors.errors || {},
-  meta: state.photos.meta || {}
+const mapStateToProps = state => ({
+	errors: state.errors.errors || {},
+	meta: state.photos.meta || {}
 });
 
 export default connect(mapStateToProps)(Photo);
